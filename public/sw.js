e6a6e636e28bb33fd91b251bd0a0aa552ddf2652
Amol Muzumdar <amol.muzumdar@gmail.com>
2026-07-13 19:09:03 -0400

    Build V3.1 public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "micro-empire-v3";
+const CACHE = "micro-empire-v3-1";
 const CORE = ["/micro-empire-game/", "/micro-empire-game/manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
