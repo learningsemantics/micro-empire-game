e198a8a6967457682bf9b45fc3b5bf1b15de0bf7
Amol Muzumdar <amol.muzumdar@gmail.com>
2026-07-13 18:32:23 -0400

    Add V3 public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+const CACHE = "micro-empire-v3";
+const CORE = ["/micro-empire-game/", "/micro-empire-game/manifest.webmanifest"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
+  }).catch(() => caches.match(event.request).then(response => response || caches.match("/micro-empire-game/"))));
+});
